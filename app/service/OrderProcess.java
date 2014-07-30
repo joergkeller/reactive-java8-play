e3da4f2b309a5a32be245e024c6009c32f281403
jke <jke@zuehlke.com>
2014-07-30 23:16:59 +0200

    The individual blocking steps are executed asynchronously, promised results are chained but in the end a block waits for completion.

--- app/service/OrderProcess.java
+++ app/service/OrderProcess.java
@@ -1,7 +1,11 @@
 package service;
 
 import java.time.temporal.Temporal;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import model.Amount;
 import model.CardDetail;
@@ -9,6 +13,8 @@ import model.Customer;
 import model.OrderItems;
 import model.SessionId;
 import model.ValidationResult;
+import play.libs.F.Promise;
+import scala.concurrent.ExecutionContext;
 import service.helper.Scheduler;
 import service.subtask.CreditCardTask;
 import service.subtask.CustomerTask;
@@ -16,9 +22,22 @@ import service.subtask.DbTask;
 import service.subtask.ItemTask;
 import service.subtask.MailTask;
 import service.subtask.OrderTask;
+import akka.dispatch.ExecutionContexts;
 
 public class OrderProcess {
 	
+	static final ExecutionContext runner = ExecutionContexts.fromExecutorService(Executors.newFixedThreadPool(8)); // non-blocking tasks
+	static final ExecutionContext dbRunner = ExecutionContexts.fromExecutorService(Executors.newFixedThreadPool(4)); // blocking db access
+	static final ExecutionContext wsRunner = ExecutionContexts.fromExecutorService(Executors.newFixedThreadPool(8)); // blocking webservices
+	static final ExecutionContext fsRunner = ExecutionContexts.fromExecutorService(Executors.newSingleThreadExecutor()); // blocking filesystem writes
+	static final ExecutionContext mailRunner = ExecutionContexts.fromExecutorService(Executors.newSingleThreadExecutor()); // blocking mail send
+
+	public static List<String> awaitAll(Collection<Promise<String>> promises) {
+		@SuppressWarnings("unchecked")
+		Promise<String>[] p = promises.toArray(new Promise[promises.size()]);
+		return Promise.sequence(runner, p).get(300, TimeUnit.SECONDS);
+	}
+
 	final SessionId sessionId;
 	final DbTask dbTask;
 	final CustomerTask customerTask;
@@ -39,72 +58,80 @@ public class OrderProcess {
 	}
 	
 	public String processOrder() {
-		return collectCurrentCustomer();
+		return collectCurrentCustomer().get(60, TimeUnit.SECONDS);
 	}
 
-	private String collectCurrentCustomer() {
-		Optional<Customer> customerOption = customerTask.obtain(sessionId);
-		return collectSelectedItems(customerOption);
+	private Promise<String> collectCurrentCustomer() {
+		Promise<Optional<Customer>> customerFuture = Promise.promise(() -> customerTask.obtain(sessionId), dbRunner);
+		customerFuture.onFailure(th -> th.printStackTrace(), fsRunner);
+		return customerFuture.flatMap(this::collectSelectedItems, runner);
 	}
 
-	private String collectSelectedItems(Optional<Customer> customerOption) {
+	private Promise<String> collectSelectedItems(Optional<Customer> customerOption) {
 		if (!customerOption.isPresent()) {
-			return onMissingCustomer();
+			return Promise.pure(onMissingCustomer());
 		}
 		Customer customer = customerOption.get();
 
-		OrderItems orderItems = itemTask.collect(sessionId);
-		return checkCardValidation(customer, orderItems);
+		Promise<OrderItems> orderFuture = Promise.promise(() -> itemTask.collect(sessionId), dbRunner);
+		orderFuture.onFailure(th -> th.printStackTrace(), fsRunner);
+		return orderFuture.flatMap(orderItems -> checkCardValidation(customer, orderItems), runner);
 	}
 
-	private String checkCardValidation(Customer customer, OrderItems orderItems) {
+	private Promise<String> checkCardValidation(Customer customer, OrderItems orderItems) {
 		if (orderItems.isEmpty()) {
-			return onMissingItems();
+			return Promise.pure(onMissingItems());
 		}
 
 		CardDetail cardDetails = customer.getCardDetails();
 		Amount totalAmount = orderItems.getTotalAmount();
-		ValidationResult validation = cardTask.checkValidity(cardDetails, totalAmount);
-		return assignOrderedItems(customer, orderItems, validation);
+		Promise<ValidationResult> validationFuture = Promise.promise(() -> cardTask.checkValidity(cardDetails, totalAmount), wsRunner);
+		validationFuture.onFailure(th -> th.printStackTrace(), fsRunner);
+		return validationFuture.flatMap(validation -> assignOrderedItems(customer, orderItems, validation), runner);
 	}
 
-	private String assignOrderedItems(Customer customer, OrderItems orderItems, ValidationResult validation) {
+	private Promise<String> assignOrderedItems(Customer customer, OrderItems orderItems, ValidationResult validation) {
 		CardDetail cardDetails = customer.getCardDetails();
 		Amount totalAmount = orderItems.getTotalAmount();
 		if (!validation.isValid()) {
-			return onInvalidCard(cardDetails, totalAmount);
+			return Promise.pure(onInvalidCard(cardDetails, totalAmount));
 		}
 		
-		boolean reservation = itemTask.assign(sessionId, orderItems);
-		return chargeTotalAmount(customer, orderItems, validation, reservation);
+		Promise<Boolean> reservationFuture = Promise.promise(() -> itemTask.assign(sessionId, orderItems), dbRunner);
+		reservationFuture.onFailure(th -> th.printStackTrace(), fsRunner);
+		return reservationFuture.flatMap(reservation -> chargeTotalAmount(customer, orderItems, validation, reservation), runner);
 	}
 
-	private String chargeTotalAmount(Customer customer, OrderItems orderItems, ValidationResult validation, boolean reservation) {
+	private Promise<String> chargeTotalAmount(Customer customer, OrderItems orderItems, ValidationResult validation, Boolean reservation) {
 		if (!reservation) {
-			return onOutOfStock();
+			return Promise.pure(onOutOfStock());
 		}
 		
 		Amount totalAmount = orderItems.getTotalAmount();
-		ValidationResult debitResult = cardTask.debit(validation.getReservation(), totalAmount);
-		return submitOrderForProcessing(customer, orderItems, debitResult);
+		Promise<ValidationResult> debitFuture = Promise.promise(() -> cardTask.debit(validation.getReservation(), totalAmount), wsRunner);
+		debitFuture.onFailure(th -> th.printStackTrace(), fsRunner);
+		return debitFuture.flatMap(debitResult -> submitOrderForProcessing(customer, orderItems, debitResult), runner);
 	}
 
-	private String submitOrderForProcessing(Customer customer, OrderItems orderItems, ValidationResult debitResult) {
+	private Promise<String> submitOrderForProcessing(Customer customer, OrderItems orderItems, ValidationResult debitResult) {
 		CardDetail cardDetails = customer.getCardDetails();
 		Amount totalAmount = orderItems.getTotalAmount();
 		if (!debitResult.isValid()) {
-			return onInvalidCard(cardDetails, totalAmount);
+			return Promise.pure(onInvalidCard(cardDetails, totalAmount));
 		}
 		
-		Temporal expectedDelivery = orderTask.submit(orderItems, customer);
-		return sendConfirmationMail(customer, orderItems, expectedDelivery);
+		Promise<Temporal> deliveryFuture = Promise.promise(() -> orderTask.submit(orderItems, customer), fsRunner);
+		deliveryFuture.onFailure(th -> th.printStackTrace(), fsRunner);
+		return deliveryFuture.flatMap(expectedDelivery -> sendConfirmationMail(customer, orderItems, expectedDelivery), runner);
 	}
 
-	private String sendConfirmationMail(Customer customer, OrderItems orderItems, Temporal expectedDelivery) {
-		mailTask.confirm(orderItems, expectedDelivery, customer);
-		return onOrderConfirmation(orderItems, expectedDelivery, customer);
+	private Promise<String> sendConfirmationMail(Customer customer, OrderItems orderItems, Temporal expectedDelivery) {
+		Promise<Boolean> mailFuture = Promise.promise(() -> mailTask.confirm(orderItems, expectedDelivery, customer), mailRunner);
+		mailFuture.onFailure(th -> th.printStackTrace(), fsRunner);
+		return Promise.pure(onOrderConfirmation(orderItems, expectedDelivery, customer));
 	}
 
+	
 	protected String onMissingCustomer() {
 		return "Missing customer information. You will be redirected to the login page.";
 	}
